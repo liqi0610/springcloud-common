7c8fdf7ae4e715a0629434af3a7a3e00e42281de
zyw090111@163.com <zyw090111@163.com>
2018-10-10 11:27:37 +0800

    添加断路由，并设置断路由的超时时间

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -3,10 +3,17 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class IndexController {
     @GetMapping("/index")
     public String index(){
+        try {
+            TimeUnit.MILLISECONDS.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return "444444444";
     }
 }
