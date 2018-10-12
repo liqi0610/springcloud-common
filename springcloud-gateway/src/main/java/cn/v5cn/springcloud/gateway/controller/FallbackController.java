635a20194b86f323675f468381187d74358b7be8
zyw090111@163.com <zyw090111@163.com>
2018-10-12 16:13:57 +0800

    设置gateway验证代码

--- springcloud-gateway/src/main/java/cn/v5cn/springcloud/gateway/controller/FallbackController.java
+++ springcloud-gateway/src/main/java/cn/v5cn/springcloud/gateway/controller/FallbackController.java
@@ -3,6 +3,9 @@ package cn.v5cn.springcloud.gateway.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 全局熔断器
+ */
 @RestController
 public class FallbackController {
 
