85cee5beef0d864b086f071d01e5dae0a9e39a7a
zyw090111@163.com <zyw090111@163.com>
2018-10-17 19:31:02 +0800

    添加受保护的服务springcloud-service

--- springcloud-service/src/main/java/cn/v5cn/springcloud/service/ServiceApplication.java
+++ springcloud-service/src/main/java/cn/v5cn/springcloud/service/ServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudServiceApplication {
+public class ServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudServiceApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
     }
 }
