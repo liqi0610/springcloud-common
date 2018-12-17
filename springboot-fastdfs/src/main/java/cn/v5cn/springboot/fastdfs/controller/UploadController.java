ead13a95cf3bfbdde3336eb80759143f6c50b0de
zyw090111UC <zyw090111@163.com>
2018-12-17 22:12:37 +0800

    FastDFS上传下载文件

--- springboot-fastdfs/src/main/java/cn/v5cn/springboot/fastdfs/controller/UploadController.java
+++ springboot-fastdfs/src/main/java/cn/v5cn/springboot/fastdfs/controller/UploadController.java
@@ -1,10 +1,12 @@
 package cn.v5cn.springboot.fastdfs.controller;
 
+import com.github.tobato.fastdfs.domain.MetaData;
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.proto.storage.DownloadCallback;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author ZYW
@@ -29,16 +34,46 @@ public class UploadController {
 
     @PostMapping("/upload")
     public ResponseEntity<String> upload(MultipartFile file) throws IOException {
-        StorePath storePath = fileStorageClient.uploadFile(file.getInputStream(), file.getSize(), FilenameUtils.getExtension(file.getOriginalFilename()), null);
+        //设置文件元数据
+        Set<MetaData> metaDataSet = new HashSet<>();
+        metaDataSet.add(new MetaData("originalFilename",file.getOriginalFilename()));
+        metaDataSet.add(new MetaData("size",String.valueOf(file.getSize())));
+        metaDataSet.add(new MetaData("fileExt",FilenameUtils.getExtension(file.getOriginalFilename())));
+        StorePath storePath = fileStorageClient.uploadFile(file.getInputStream()
+                , file.getSize()
+                , FilenameUtils.getExtension(file.getOriginalFilename())
+                , metaDataSet);
 
         return ResponseEntity.ok(storePath.getFullPath() + "=====" + storePath.getPath() + "+++" + storePath.getGroup());
     }
 
     @PostMapping("/download")
     public void download(String groupName, String path, HttpServletResponse response) {
+        //获取文件元数据
+        Set<MetaData> metadataSet = fileStorageClient.getMetadata(groupName, path);
+        String origName = metadataSet.stream()
+                .filter(item -> StringUtils.equals(item.getName(), "originalFilename"))
+                .findFirst()
+                .orElse(new MetaData("originalFilename", ""))
+                .getValue();
         fileStorageClient.downloadFile(groupName,path, (is) -> {
-            IOUtils.copy(is,response.getOutputStream());
-            return null;
+            ServletOutputStream responseOutputStream = null;
+            try {
+                // 设置response参数，可以打开下载页面
+                response.reset();
+                response.setContentType("binary/octet-stream;charset=UTF-8");
+                response.setHeader("Content-Disposition",
+                        "attachment;filename=" + new String(origName.getBytes(), "iso-8859-1"));
+                responseOutputStream = response.getOutputStream();
+                IOUtils.copy(is,responseOutputStream);
+                responseOutputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                IOUtils.closeQuietly(is);
+                IOUtils.closeQuietly(responseOutputStream);
+            }
+            return Void.TYPE;
         });
     }
 
