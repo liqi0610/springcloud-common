28da6111a99e5817f03b6aae0182bac5fea9ca1c
zyw090111@163.com <zyw090111@163.com>
2018-12-27 14:04:59 +0800

    RabbitMQ 延迟队列

--- springboot-rabbitmq/src/main/java/cn/v5cn/springboot/rabbitmq/config/DelayRabbitMQConfig.java
+++ springboot-rabbitmq/src/main/java/cn/v5cn/springboot/rabbitmq/config/DelayRabbitMQConfig.java
@@ -4,13 +4,11 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <p>
- *     延时队列的创建
+ * 延时队列的创建
  * </p>
+ *
  * @author ZYW
  * @version 1.0
  * @date 2018/12/27 11:39
@@ -24,21 +22,29 @@ public class DelayRabbitMQConfig {
 
     public static final String X_DELAYED_MESSAGE = "x-delayed-message";
 
+    public static final String X_DELAYED_TYPE = "x-delayed-type";
+
     @Bean("delayExchange")
-    public CustomExchange delayExchange() {
-        /*Map<String, Object> args = new HashMap<String, Object>();
-        args.put("x-delayed-type", "direct");
-        return new CustomExchange(EXCHANGE_NAME, X_DELAYED_MESSAGE, true, false, args);*/
-        return (CustomExchange)new ExchangeBuilder(EXCHANGE_NAME,X_DELAYED_MESSAGE).durable(true).withArgument("x-delayed-type","direct").build();
+    public Exchange delayExchange() {
+        return ExchangeBuilder.directExchange(EXCHANGE_NAME)
+                .delayed()
+                .durable(true)
+                .withArgument(X_DELAYED_TYPE, "direct")
+                .build();
     }
 
     @Bean("delayQueue")
     public Queue delayQueue() {
-        return QueueBuilder.durable(QUEUE_NAME).build();
+        return QueueBuilder
+                .durable(QUEUE_NAME)
+                .build();
     }
 
     @Bean("delayExchangeQueueBinding")
-    public Binding delayExchangeQueueBinding(CustomExchange delayExchange, Queue delayQueue) {
-        return BindingBuilder.bind(delayQueue).to(delayExchange).with(QUEUE_NAME).noargs();
+    public Binding delayExchangeQueueBinding(Exchange delayExchange, Queue delayQueue) {
+        return BindingBuilder
+                .bind(delayQueue)
+                .to(delayExchange)
+                .with(QUEUE_NAME).noargs();
     }
 }
