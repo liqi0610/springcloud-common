a2f0aa4ea323bc5aabb72edcb5059ae9a40edce7
AS <zyw090111@163.com>
2019-07-10 17:09:00 +0800

    添加j2Cache demo

--- springboot-cache-j2cache/src/main/java/cn/v5cn/springboot/cache/j2cache/controller/IndexController.java
+++ springboot-cache-j2cache/src/main/java/cn/v5cn/springboot/cache/j2cache/controller/IndexController.java
@@ -1,5 +1,9 @@
 package cn.v5cn.springboot.cache.j2cache.controller;
 
+import cn.v5cn.springboot.cache.j2cache.entity.User;
+import net.oschina.j2cache.CacheChannel;
+import net.oschina.j2cache.CacheObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Autowired
+    private CacheChannel cacheChannel;
+
     @GetMapping("/index")
     public Object index(){
-        return null;
+        CacheObject cacheObject = cacheChannel.get("default", "user");
+        User user = null;
+        if(cacheObject.getValue() == null) {
+            user = new User();
+            user.setId(1234L);
+            user.setName("张三");
+            user.setAge(19);
+            cacheChannel.set("default","user",user);
+            System.out.println("设置了！");
+        } else {
+            byte level = cacheObject.getLevel();
+            System.out.println(Integer.valueOf(level));
+            user = (User)cacheObject.getValue();
+        }
+
+        return user;
     }
 
 }
