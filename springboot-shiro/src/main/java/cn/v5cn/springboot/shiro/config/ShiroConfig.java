9017eac8cec5fd28a497230d251d5e96670b4fd6
zyw090111 <zyw090111@163.com>
2019-09-01 21:29:41 +0800

    springboot shiro

---
+++ springboot-shiro/src/main/java/cn/v5cn/springboot/shiro/config/ShiroConfig.java
@@ -0,0 +1,45 @@
+package cn.v5cn.springboot.shiro.config;
+
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author ZYW
+ * @version 1.0
+ * @date 2019-09-01 21:27
+ */
+@Configurable
+public class ShiroConfig {
+
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
+        // 由于demo1展示统一使用注解做访问控制，所以这里配置所有请求路径都可以匿名访问
+        chain.addPathDefinition("/**", "anon"); // all paths are managed via annotations
+
+        // 这另一种配置方式。但是还是用上面那种吧，容易理解一点。
+        // or allow basic authentication, but NOT require it.
+        // chainDefinition.addPathDefinition("/**", "authcBasic[permissive]");
+        return chain;
+    }
+
+    /**
+     * 解决spring aop和注解配置一起使用的bug。如果您在使用shiro注解配置的同时，引入了spring aop的starter，
+     * 会有一个奇怪的问题，导致shiro注解的请求，不能被映射，需加入以下配置：
+     * @return
+     */
+    @Bean
+    public static DefaultAdvisorAutoProxyCreator getDefaultAdvisorAutoProxyCreator(){
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator=new DefaultAdvisorAutoProxyCreator();
+        /**
+         * setUsePrefix(false)用于解决一个奇怪的bug。在引入spring aop的情况下。
+         * 在@Controller注解的类的方法中加入@RequiresRole等shiro注解，会导致该方法无法映射请求，导致返回404。
+         * 加入这项配置能解决这个bug
+         */
+        defaultAdvisorAutoProxyCreator.setUsePrefix(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
+}
