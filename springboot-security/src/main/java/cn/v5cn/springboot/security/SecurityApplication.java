fc0600c9a2ae32636789576e3c399c50887ce436
AS <zyw090111@163.com>
2019-09-06 15:56:57 +0800

    spring security

---
+++ springboot-security/src/main/java/cn/v5cn/springboot/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package cn.v5cn.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
