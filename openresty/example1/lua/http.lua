59b8f56036ccdbe3b2498fb33a8713f212222afc
zyw <zyw090111@163.com>
2019-09-16 15:53:08 +0800

    完成init_worker_by_lua_*使用cosocket调用网络服务

--- openresty/example1/lua/http.lua
+++ openresty/example1/lua/http.lua
@@ -10,19 +10,17 @@ local http = require "resty.http"
 local _M = {
     _VERSION = "1.0"
 }
+
 function _M.get(url,params)
     local httpc = http.new()
     local res, err = httpc:request_uri(url, {
         method = "GET",
-        headers = {
-            ["Content-Type"] = "application/x-www-form-urlencoded",
-        },
         keepalive_timeout = 60,
         keepalive_pool = 10
     })
 
     if not res then
-        ngx.say("failed to request: ", err)
+        ngx.log(ngx.DEBUG,"failed to request: ", err)
         return
     end
 
@@ -30,13 +28,13 @@ function _M.get(url,params)
     -- all in one go, including any trailers, and the connection closed or keptalive
     -- for you.
 
-    ngx.status = res.status
+    -- ngx.status = res.status
 
     for k,v in pairs(res.headers) do
         ngx.log(ngx.DEBUG,"header: ",k,v)
     end
 
-    ngx.say(res.body)
+    ngx.log(ngx.DEBUG,"body: ",res.body)
 end
 
 return _M
