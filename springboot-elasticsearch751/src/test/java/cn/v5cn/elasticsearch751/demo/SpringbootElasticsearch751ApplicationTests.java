8e73fde30910052ad2269719937c8378bdb5df25
AS <zyw090111@163.com>
2020-01-19 17:59:31 +0800

    添加Elasticsearch demo

---
+++ springboot-elasticsearch751/src/test/java/cn/v5cn/elasticsearch751/demo/SpringbootElasticsearch751ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.v5cn.elasticsearch751.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearch751ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
