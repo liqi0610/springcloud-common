85a1f4fb6e96d63d41d24f97efcf4167172e3a82
zyw090111 <zyw090111@163.com>
2020-02-21 22:41:09 +0800

    自定义Sink

--- flink19-demo/flink19-example/src/main/java/cn/v5cn/flink19/transformations/KeyByTransformation3.java
+++ flink19-demo/flink19-example/src/main/java/cn/v5cn/flink19/transformations/KeyByTransformation3.java
@@ -2,6 +2,7 @@ package cn.v5cn.flink19.transformations;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -11,7 +12,8 @@ import org.apache.flink.util.Collector;
 import java.util.Arrays;
 
 /**
- * keyBy传入多值，类似sql的grup by后面跟多个字段，可以根据多个字段进行分组
+ * keyBy传入多值，类似sql的grup by后面跟多个字段，可以根据多个字段进行分组。
+ * 例子是各个省内市的GDP，单位千亿
  * @author ZYW
  * @version 1.0
  * @date 2020-02-20 21:38
@@ -22,32 +24,31 @@ public class KeyByTransformation3 {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         //source
-        DataStreamSource<String> lines = env.fromElements("hello you","hello me","hello world");
+        DataStreamSource<String> lines = env.fromElements("陕西,西安,8","陕西,西安,7","河北,石家庄,4","河北,石家庄,3");
 
-        //Map Transformation是输入一个参数，经过map方法在产生一个参数。
-        SingleOutputStreamOperator<WordCount> result = lines.flatMap(new FlatMapFunction<String, WordCount>() {
+        //flatMap Transformation是输入一个参数，不产生参数
+        SingleOutputStreamOperator<Tuple3<String,String,Integer>> result = lines.flatMap(new FlatMapFunction<String, Tuple3<String,String,Integer>>() {
             @Override
-            public void flatMap(String line, Collector<WordCount> out) throws Exception {
-                Arrays.stream(line.split(" ")).forEach(word -> {
-                        out.collect(new WordCount(word,1));
-                });
+            public void flatMap(String line, Collector<Tuple3<String,String,Integer>> out) throws Exception {
+                String[] words = line.split(",");
+                out.collect(Tuple3.of(words[0],words[1],Integer.parseInt(words[2])));
             }
         });
 
-        //需要使用keyBy的重载方法完成，通过属性名称完成，内部使用反射
-        KeyedStream<WordCount, Tuple> keyed = result.keyBy("word");
-        //sum聚合方法也有同样的重载方法。
-        SingleOutputStreamOperator<WordCount> summd = keyed.sum("counts");
+        //KeyBy可以根据多个参数进行分组
+        KeyedStream<Tuple3<String, String, Integer>, Tuple> keyByed = result.keyBy(0, 1);
+        //sum聚合方法。
+        SingleOutputStreamOperator<Tuple3<String, String, Integer>> summd = keyByed.sum(2);
 
         //Sink
         summd.print();
 
-        env.execute("KeyByTransformation2");
+        env.execute("KeyByTransformation3");
 
-//        输出结果，是单词和数量
-//        2> WordCount{word='hello', counts=1}
-//        2> WordCount{word='me', counts=1}
-//        3> WordCount{word='you', counts=1}
-//        2> WordCount{word='hello', counts=2}
+//        输出结果
+//        3> (陕西,西安,8)
+//        3> (陕西,西安,15)
+//        4> (河北,石家庄,4)
+//        4> (河北,石家庄,7)
     }
 }
