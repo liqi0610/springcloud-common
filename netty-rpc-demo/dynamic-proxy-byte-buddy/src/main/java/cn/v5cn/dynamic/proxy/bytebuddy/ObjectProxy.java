e3fd70eafd9d51261ab2000b703267893c877d8a
zyw090111 <zyw090111@163.com>
2020-03-02 22:23:33 +0800

    byte buddy代理的简单例子

--- netty-rpc-demo/dynamic-proxy-byte-buddy/src/main/java/cn/v5cn/dynamic/proxy/bytebuddy/ObjectProxy.java
+++ netty-rpc-demo/dynamic-proxy-byte-buddy/src/main/java/cn/v5cn/dynamic/proxy/bytebuddy/ObjectProxy.java
@@ -19,6 +19,17 @@ public class ObjectProxy {
                 .load(ObjectProxy.class.getClassLoader())
                 .getLoaded();
 
+        System.out.println("所在包：" + dynamicType.getPackage().getName());
+        System.out.println("类名称：" + dynamicType.getName());
+
         System.out.println(dynamicType.newInstance().toString());
+
+//        没有指定类名称，byte buddy会随机生成一个类名称，规则如下输出。
+//        包名称生成规则是，如果父类不是java.lang包的类，那么包名称就是跟父类在同一个包下，如果是java.lang包的类，
+//        规则如下输出，在包名称前会添加net.bytebuddy.renamed前缀
+//        上面输出：
+//        所在包：net.bytebuddy.renamed.java.lang
+//        类名称：net.bytebuddy.renamed.java.lang.Object$ByteBuddy$15EdYfqM
+//        Hello World!
     }
 }
