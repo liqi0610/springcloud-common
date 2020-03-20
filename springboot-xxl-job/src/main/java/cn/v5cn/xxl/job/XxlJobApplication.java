195079acdbd96d0586c7f860d4e82ef449367d0c
zyw090111 <zyw090111@163.com>
2020-03-20 22:43:53 +0800

    xxl-job demo

---
+++ springboot-xxl-job/src/main/java/cn/v5cn/xxl/job/XxlJobApplication.java
@@ -0,0 +1,13 @@
+package cn.v5cn.xxl.job;
+
+/**
+ * @author ZYW
+ * @version 1.0
+ * @date 2020-03-20 22:43
+ */
+
+public class XxlJobApplication {
+    public static void main(String[] args) {
+
+    }
+}
